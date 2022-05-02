9388a425c6411f5501cbe34eb139c3b176ce70aa
Lukas Dorner <lukasdorner1@gmail.com>
2022-05-02 12:43:03 +0200

    introduce imgconv pkg

---
+++ imgconv/converter.go
@@ -0,0 +1,38 @@
+package imgconv
+
+import (
+	"image"
+	"image/color"
+)
+
+// ToNRGBA converts any image m to an *image.NRGBA image.
+// Any Image may be converted, but images that are not image.NRGBA might be converted lossily.
+func ToNRGBA(m image.Image) *image.NRGBA {
+	img := image.NewNRGBA(m.Bounds())
+
+	for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+		for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+			px := m.At(x, y)
+			px = color.NRGBAModel.Convert(px)
+			img.Set(x, y, px)
+		}
+	}
+
+	return img
+}
+
+// ToRGBA converts any image m to an *image.RGBA image.
+// Any Image may be converted, but images that are not image.RGBA might be converted lossily.
+func ToRGBA(m image.Image) *image.RGBA {
+	img := image.NewRGBA(m.Bounds())
+
+	for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
+		for y := m.Bounds().Min.Y; y < m.Bounds().Max.Y; y++ {
+			px := m.At(x, y)
+			px = color.RGBAModel.Convert(px)
+			img.Set(x, y, px)
+		}
+	}
+
+	return img
+}
