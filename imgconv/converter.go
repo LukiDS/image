952dbf79b406feb09aeb1503a902319e91f0ee02
Lukas Dorner <lukasdorner1@gmail.com>
2022-05-04 14:53:27 +0200

    improve image converter

--- imgconv/converter.go
+++ imgconv/converter.go
@@ -8,6 +8,10 @@ import (
 // ToNRGBA converts any image m to an *image.NRGBA image.
 // Any Image may be converted, but images that are not image.NRGBA might be converted lossily.
 func ToNRGBA(m image.Image) *image.NRGBA {
+	if m.ColorModel() == color.NRGBAModel {
+		return m.(*image.NRGBA)
+	}
+
 	img := image.NewNRGBA(m.Bounds())
 
 	for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
@@ -24,6 +28,10 @@ func ToNRGBA(m image.Image) *image.NRGBA {
 // ToRGBA converts any image m to an *image.RGBA image.
 // Any Image may be converted, but images that are not image.RGBA might be converted lossily.
 func ToRGBA(m image.Image) *image.RGBA {
+	if m.ColorModel() == color.RGBAModel {
+		return m.(*image.RGBA)
+	}
+
 	img := image.NewRGBA(m.Bounds())
 
 	for x := m.Bounds().Min.X; x < m.Bounds().Max.X; x++ {
